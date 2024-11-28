08d19c7bd08a81854ca66b6b2a4c2e119ef25281
ezmilyy31 <tlatjdus0531@naver.com>
2024-11-28 02:17:29 +0100

    SY 11/28 02:17 Modify Farmer.java & Add Inventories

--- Farm-Simulator-Game-master/Farm-Simulator-Game-master/src/main/Farmer.java
+++ Farm-Simulator-Game-master/Farm-Simulator-Game-master/src/main/Farmer.java
@@ -1,72 +1,176 @@
 package main;
 
+import java.util.HashMap;
+import java.util.Map;
+import main.Crop;
+import main.Item;
+
 /**
  * Farmer class where the users farmer is.
- * Here the users farmer gets set up and where the program gets the users age from.
- * @author Griffin Baxter and Rutger van Kruiningen
+ * Here the users farmer gets set up.
  */
+
 public class Farmer 
 {
 
-	/**
-	 * The name of the farmer.
-	 */
+	//The name of the farmer.
 	private String farmerName;
 	
-	/**
-	 * The age of the farmer.
-	 */
-	private int farmerAge;
+	//The Strength of the farmer.
+	private int farmerStrength;
+	
+	//The days passed of the farmer on the farm, starts at 1.
+	private int farmerDays;
+	
+	//The inventory for crops
+	private Map<String, Integer> cropInventory = new HashMap<>();
+	
+	//The inventory for items
+	private Map<String, Integer> itemInventory = new HashMap<>();
 	
 	/**
-	 * The days passed of the farmer on the farm, starts at 1.
+	 * Constructor function for Farmer Class
+	 * Name: Name of the farmer.
+	 * Days: The Days passed while farming.
+	 * Strength: The Strength of the farmer.
 	 */
-	private int farmerDaysPassed;
-	
-	/**
-	 * Constructor function for Farmer Class, this constructor initialises variables <code>farmerName</code>, <code>farmerAge</code>.
-	 * @param name Name of the farmer.
-	 * @param daysPassed The days passed for the farmer.
-	 * @param age Age of the farmer.
-	 */
-	public Farmer(String name, int daysPassed, int age)
+	public Farmer(String name, int strength, int days)
 	{
 		farmerName = name;
-		farmerDaysPassed = daysPassed;
-		farmerAge = age;
+		farmerStrength = strength;
+		farmerDays = days;
+		
+		//Initialize cropInventory
+		cropInventory.put("Cabbage", 0);
+		cropInventory.put("Garlic", 0);
+		cropInventory.put("Pepper", 0);
+		cropInventory.put("Rice", 0);
+		cropInventory.put("Wheat", 0);
+		
+		//Initialize itemInventory
+		itemInventory.put("Compost", 0);
+		itemInventory.put("Egg", 0);
+		itemInventory.put("Namool", 0);
+		itemInventory.put("RedBull", 0);
 	}
 	
-	/**
-	 * Function to increase the days passed of the farmer on the farm, increases the days passed by 1.
-	 */
+	//Function to increase the days passed of the farmer on the farm, increases the days passed by 1
 	public void increaseDaysPassed()
 	{
-		farmerDaysPassed++;
+		farmerDays++;
 	}
 	
-	/**
-	 * Returns the farmer's age.
-	 * @return The farmer's age.
-	 */
-	public int getFarmerAge()
+	//Returns the farmer's strength.
+	public int getFarmerStrength()
 	{
-		return farmerAge;
+		return farmerStrength;
 	}
 	
-	/**
-	 * Returns the days passed for the farmer.
-	 * @return The days passed for the farmer.
-	 */
-	public int getDaysPassed()
+	//Returns the days passed for the farmer.
+	public int getDays()
 	{
-		return farmerDaysPassed;
+		return farmerDays;
 	}
 	
-	/**
-	 * Returns the farmer's name.
-	 * @return The farmer's name.
-	 */
-	public String getFarmerName() {
+	//Returns the farmer's name.
+	public String getFarmerName()
+	{
 		return farmerName;
+	}
+	
+	//Returns Inventory for Crops
+	public Map<String, Integer> getCropInven()
+	{
+		return cropInventory;
+	}
+	
+	//Returns Inventory for Items
+	public Map<String, Integer> getItemInven()
+	{
+		return itemInventory;
+	}
+	
+	//Add an Item in Crop Inventory
+	public void addCropInven(Crop crop)
+	{
+		String name = crop.getName();
+		if(cropInventory.containsKey(name))
+		{
+			cropInventory.put(name, cropInventory.get(name)+1);
+		}
+		return;
+	}
+	
+	//Add an Item in Item Inventory
+	public void addItemInven(Item item)
+	{
+		String name = item.getName();
+		if(itemInventory.containsKey(name))
+		{
+			itemInventory.put(name, itemInventory.get(name)+1);
+		}
+		return;
+	}
+	
+	//Remove an Item from Crop Inventory
+	public void subCropInven(Crop crop)
+	{
+		String name = crop.getName();
+		if(cropInventory.containsKey(name))
+		{
+			Integer value = getCropValue(crop);
+			if(value != null && value > 0)
+			{
+				cropInventory.put(name, value-1);
+			}
+			else
+			{
+				cropInventory.put(name, 0);
+			}
+		}
+		return;
+	}
+	
+	//Remove an Item from Item Inventory
+	public void subItemInven(Item item)
+	{
+		String name = item.getName();
+		if(itemInventory.containsKey(name))
+		{
+			Integer value = getItemValue(item);
+			if(value != null && value > 0)
+			{
+				itemInventory.put(name, value-1);
+			}
+			else
+			{
+				itemInventory.put(name, 0);
+			}
+		}
+		return;
+	}
+	
+	//Returns value of the crop (when it doesn't exist -> null)
+	public Integer getCropValue(Crop crop) 
+	{
+		String name = crop.getName();
+		if(cropInventory.containsKey(name))
+		{
+			return cropInventory.get(name);
+		}
+		else
+			return null;
+	}
+	
+	//Returns value of the item (when it doesn't exist -> null)
+	public Integer getItemValue(Item item) 
+	{
+		String name = item.getName();
+		if(itemInventory.containsKey(name))
+		{
+			return itemInventory.get(name);
+		}
+		else
+			return null;
 	}
 }
